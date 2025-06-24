c6343e247c9708e6975aaaedc3d43fa36c2f40b4
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-06-24 15:36:08 +0900

    클라이언트 컴포넌트에서 server action 사용 & useActionState사용 시 input defaultValue 값 설정

---
+++ src/app/(beforeLogin)/_lib/signup.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { redirect } from "next/navigation";
+type State = {
+  message: string | null;
+  id?: string;
+  name?: string;
+  password?: string;
+};
+
+const onSubmit = async (
+  prevState: State,
+  formData: FormData
+): Promise<State> => {
+  const id = (formData.get("id") as string)?.trim() ?? "";
+  const name = (formData.get("name") as string)?.trim() ?? "";
+  const password = (formData.get("password") as string)?.trim() ?? "";
+  const image = formData.get("image");
+
+  console.log(image, "!!");
+  if (!id) return { message: "no_id", id, name, password };
+  if (!name) return { message: "no_name", id, name, password };
+  if (!password) return { message: "no_password", id, name, password };
+  if (!image) return { message: "no_image", id, name, password };
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/users`,
+      {
+        method: "post",
+        body: formData,
+        credentials: "include",
+      }
+    );
+    if (response.status === 403) {
+      return { message: "user_exists", id, name, password };
+    }
+
+    // 성공 시 이동
+    redirect("/home");
+  } catch (err) {
+    console.error(err);
+  }
+
+  // 실패했지만 오류 없음 → 그대로 반환
+  return { message: null, id, name, password };
+};
+
+export default onSubmit;
