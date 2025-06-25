c9b15807022273edc32f75b1a32baa3a7aff4dcf
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-06-25 18:21:42 +0900

    react quert ssr 설정

---
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function RQProvider({ children }: Props) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={process.env.NEXT_PUBLIC_MODE === "local"}
+      />
+    </QueryClientProvider>
+  );
+}
