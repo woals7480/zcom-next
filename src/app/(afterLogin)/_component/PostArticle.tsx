e913a89a209db90e4d94f319af779f921c3840f5
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-07-06 17:02:20 +0900

    좋아요 버튼 optimistic update 적용

--- src/app/(afterLogin)/_component/PostArticle.tsx
+++ src/app/(afterLogin)/_component/PostArticle.tsx
@@ -1,32 +1,32 @@
 "use client";
 
-import {ReactNode} from "react";
-import style from './post.module.css';
-import {useRouter} from "next/navigation";
+import { ReactNode } from "react";
+import style from "./post.module.css";
+import { useRouter } from "next/navigation";
 
 type Props = {
-  children: ReactNode,
+  children: ReactNode;
   post: {
     postId: number;
-    content: string,
+    content: string;
     User: {
-      id: string,
-      nickname: string,
-      image: string,
-    },
-    createdAt: Date,
-    Images: any[],
-  }
-}
+      id: string;
+      nickname: string;
+      image: string;
+    };
+    createdAt: Date;
+    Images: any[];
+  };
+};
 
-export default function PostArticle({ children, post}: Props) {
+export default function PostArticle({ children, post }: Props) {
   const router = useRouter();
   const onClick = () => {
     router.push(`/${post.User.id}/status/${post.postId}`);
-  }
+  };
 
   return (
-    <article onClickCapture={onClick} className={style.post}>
+    <article onClick={onClick} className={style.post}>
       {children}
     </article>
   );
