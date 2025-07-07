62f6a799e73e24edf2030ae402942a96d777091d
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-07-07 18:18:27 +0900

    메타데이터

--- src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost.ts
+++ src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost.ts
@@ -1,8 +1,10 @@
-export const getSinglePost = async ({
-  queryKey,
-}: {
-  queryKey: [string, string];
-}) => {
+import { Post } from "@/model/Post";
+import { QueryFunction } from "@tanstack/react-query";
+
+export const getSinglePost: QueryFunction<
+  Post,
+  [_1: string, _2: string]
+> = async ({ queryKey }) => {
   const [_1, id] = queryKey;
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${id}`,
--- src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePostServer.ts
+++ src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePostServer.ts
@@ -2,10 +2,11 @@ import { Post } from "@/model/Post";
 import { QueryFunction } from "@tanstack/react-query";
 import { cookies } from "next/headers";
 
-export const getSinglePostServer: QueryFunction<
-  Post,
-  [_1: string, _2: string]
-> = async ({ queryKey }) => {
+export const getSinglePostServer = async ({
+  queryKey,
+}: {
+  queryKey: [string, string];
+}) => {
   const [_1, id] = queryKey;
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${id}`,
