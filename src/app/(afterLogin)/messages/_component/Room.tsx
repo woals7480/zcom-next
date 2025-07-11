5ca772b8228838dcee8afee176d2cdf5ac1891ab
woals7480 <woals7480@gmail.com>
2025-07-11 23:02:32 +0900

    메세지 리스트 불러오기

--- src/app/(afterLogin)/messages/_component/Room.tsx
+++ src/app/(afterLogin)/messages/_component/Room.tsx
@@ -6,43 +6,38 @@ import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
+import { Room } from "@/model/Room";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
-export default function Room() {
+type Props = {
+  room: Room;
+};
+
+export default function Room({ room }: Props) {
   const router = useRouter();
-  const user = {
-    id: "hero",
-    nickname: "영웅",
-    Messages: [
-      { roomId: 123, content: "안녕하세요.", createdAt: new Date() },
-      { roomId: 123, content: "안녕히가세요.", createdAt: new Date() },
-    ],
-  };
 
   const onClick = () => {
-    router.push(`/messages/${user.Messages.at(-1)?.roomId}`);
+    router.push(`/messages/${room?.room}`);
   };
 
   return (
     <div className={style.room} onClickCapture={onClick}>
       <div className={style.roomUserImage}>
-        <img src={faker.image.avatar()} alt="" />
+        <img src={room?.Receiver.image} alt="" />
       </div>
       <div className={style.roomChatInfo}>
         <div className={style.roomUserInfo}>
-          <b>{user.nickname}</b>
+          <b>{room?.Receiver.nickname}</b>
           &nbsp;
-          <span>@{user.id}</span>
+          <span>@{room?.Receiver.id}</span>
           &nbsp; · &nbsp;
           <span className={style.postDate}>
-            {dayjs(user.Messages?.at(-1)?.createdAt).fromNow(true)}
+            {dayjs(room?.createdAt).fromNow(true)}
           </span>
         </div>
-        <div className={style.roomLastChat}>
-          {user.Messages?.at(-1)?.content}
-        </div>
+        <div className={style.roomLastChat}>{room?.content}</div>
       </div>
     </div>
   );
